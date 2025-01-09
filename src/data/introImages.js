8bb0be904504a36b1a2e17eca30e52966875fa53
gabryellateixeira <gabryellat4@gmail.com>
2025-01-09 02:26:57 +0000

    Updated images of intro

--- src/data/introImages.js
+++ src/data/introImages.js
@@ -4,44 +4,44 @@ export const introImages = [
       id: 0,
       layoutId: "animal-0",
       label: "monkey",
-      img: "/img/6.jpg",
+      img: "/img/5.jpg",
     },
     {
       id: 1,
       layoutId: "animal-1",
       label: "monkey2",
-      img: "/img/12.jpg",
+      img: "/img/5.jpg",
     },
     {
       id: 2,
       layoutId: "animal-2",
       label: "monkey3",
-      img: "/img/8.jpg",
+      img: "/img/5.jpg",
     },
     {
       id: 3,
       layoutId: "animal-3",
       label: "monkey4",
-      img: "/img/15.jpg",
+      img: "/img/5.jpg",
     },
     {
       id: 4,
       layoutId: "animal-4",
       label: "monkey5",
-      img: "/img/3.jpg",
+      img: "/img/5.jpg",
     },
     {
       id: 5,
       layoutId: "animal-5",
       label: "monkey6",
-      img: "/img/9.jpg",
+      img: "/img/5.jpg",
     },
     {
       id: 6,
       layoutId: "animal-6",
       label: "monkey7",
 
-      img: "/img/4.jpg",
+      img: "/img/5.jpg",
     },
   ],
   [
@@ -50,49 +50,49 @@ export const introImages = [
       layoutId: "animal-7",
       label: "elephant",
 
-      img: "/img/11.jpg",
+      img: "/img/blue.jpg",
     },
     {
       id: 8,
       layoutId: "animal-8",
       label: "giraffe",
 
-      img: "/img/7.jpg",
+      img: "/img/snake.jpg",
     },
     {
       id: 9,
       layoutId: "animal-9",
       label: "tiger",
 
-      img: "/img/16.jpg",
+      img: "/img/yellow.jpg",
     },
     {
       id: 10,
       layoutId: "animal-10",
       label: "lion",
 
-      img: "/img/2.jpg",
+      img: "/img/turquoise.jpg",
     },
     {
       id: 11,
       layoutId: "animal-11",
       label: "zebra",
 
-      img: "/img/13.jpg",
+      img: "/img/hero.jpg",
     },
     {
       id: 12,
       layoutId: "animal-12",
       label: "panda",
 
-      img: "/img/5.jpg",
+      img: "/img/tapir.jpg",
     },
     {
       id: 13,
       layoutId: "animal-13",
       label: "koala",
 
-      img: "/img/10.jpg",
+      img: "/img/20.jpg",
     },
   ],
   [
@@ -101,21 +101,21 @@ export const introImages = [
       layoutId: "animal-14",
       label: "kangaroo",
 
-      img: "/img/14.jpg",
+      img: "/img/liontamarin.jpg",
     },
     {
       id: 15,
       layoutId: "animal-15",
       label: "penguin",
 
-      img: "/img/1.jpg",
+      img: "/img/mountaintoucan.jpg",
     },
     {
       id: 16,
       layoutId: "animal-16",
       label: "dolphin",
 
-      img: "/img/17.jpg",
+      img: "/img/18.jpg",
     },
     {
       id: 17,
@@ -129,21 +129,21 @@ export const introImages = [
       layoutId: "animal-18",
       label: "whale",
 
-      img: "/img/19.jpg",
+      img: "/img/8.jpg",
     },
     {
       id: 19,
       layoutId: "animal-19",
       label: "eagle",
 
-      img: "/img/4.jpg",
+      img: "/img/5.jpg",
     },
     {
       id: 20,
       layoutId: "animal-20",
       label: "hawk",
 
-      img: "/img/8.jpg",
+      img: "/img/20.jpg",
     },
   ],
   [
@@ -159,19 +159,19 @@ export const introImages = [
       layoutId: "animal-22",
       label: "snake",
 
-      img: "/img/12.jpg",
+      img: "/img/glass.jpg",
     },
     {
       id: 23,
       layoutId: "animal-23",
       label: "lizard",
-      img: "/img/9.jpg",
+      img: "/img/jaguar.jpg",
     },
     {
       id: 24,
       layoutId: "animal-24",
       label: "turtle",
-      img: "/img/3.jpg",
+      img: "/img/caiman.jpg",
     },
     {
       id: 25,
@@ -189,7 +189,7 @@ export const introImages = [
       id: 27,
       layoutId: "animal-27",
       label: "wolf",
-      img: "/img/9.jpg",
+      img: "/img/macaw.jpg",
     },
   ],
   [
@@ -197,13 +197,13 @@ export const introImages = [
       id: 28,
       layoutId: "animal-28",
       label: "bear",
-      img: "/img/2.jpg",
+      img: "/img/5.jpg",
     },
     {
       id: 29,
       layoutId: "animal-29",
       label: "raccoon",
-      img: "/img/11.jpg",
+      img: "/img/5.jpg",
     },
     {
       id: 30,
@@ -215,25 +215,25 @@ export const introImages = [
       id: 31,
       layoutId: "animal-31",
       label: "deer",
-      img: "/img/13.jpg",
+      img: "/img/5.jpg",
     },
     {
       id: 32,
       layoutId: "animal-32",
       label: "rabbit",
-      img: "/img/16.jpg",
+      img: "/img/5.jpg",
     },
     {
       id: 33,
       layoutId: "animal-33",
       label: "mouse",
-      img: "/img/10.jpg",
+      img: "/img/5.jpg",
     },
     {
       id: 34,
       layoutId: "animal-34",
       label: "cat",
-      img: "/img/1.jpg",
+      img: "/img/5.jpg",
     },
   ],
 ];
